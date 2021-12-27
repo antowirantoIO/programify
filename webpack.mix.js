9ec6f8483e6dbad636f38aa593c03f618fb2e485
Anto Wiranto <antowrnto11@gmail.com>
2021-12-27 15:05:27 +0000

    Install authentication with inertiajs and reactjs

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+  .ts('resources/js/app.tsx', 'public/js')
+  .react()
+  .postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ])
+  .webpackConfig(require('./webpack.config'));
+
+if (mix.inProduction()) {
+  mix.version();
+}
