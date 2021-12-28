4086e4df7e9da534cd415d47f366fc18f12ab7a3
Anto Wiranto <antowrnto11@gmail.com>
2021-12-28 04:13:02 +0000

    Create routes and controllers

---
+++ app/Http/Controllers/WelcomeContoller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+class WelcomeContoller extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,77 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\DashboardContoller;
+use App\Http\Controllers\PathController;
+use App\Http\Controllers\SkillController;
+use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\EpisodeController;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\WelcomeContoller;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', WelcomeContoller::class)->name('welcome');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/dashboard', DashboardContoller::class)->name('dashboard');
+
+    Route::prefix('path')->name('path.')->group(function () {
+        Route::get('/', [PathController::class, 'index'])->name('index');
+        Route::get('/create', [PathController::class, 'create'])->name('create');
+        Route::post('/store', [PathController::class, 'store'])->name('store');
+        Route::get('/{path:slug}', [PathController::class, 'show'])->name('show');
+        Route::get('/edit/{path:slug}', [PathController::class, 'edit'])->name('edit');
+        Route::patch('/update/{path:slug}', [PathController::class, 'update'])->name('index');
+        Route::delete('/destroy/{path:slug}', [PathController::class, 'destroy'])->name('index');
+    });
+
+    Route::prefix('skill')->name('skill.')->group(function () {
+        Route::get('/', [SkillController::class, 'index'])->name('index');
+        Route::get('/create', [SkillController::class, 'create'])->name('create');
+        Route::post('/store', [SkillController::class, 'store'])->name('store');
+        Route::get('/{skill:slug}', [SkillController::class, 'show'])->name('show');
+        Route::get('/edit/{skill:slug}', [SkillController::class, 'edit'])->name('edit');
+        Route::patch('/update/{skill:slug}', [SkillController::class, 'update'])->name('index');
+        Route::delete('/destroy/{skill:slug}', [SkillController::class, 'destroy'])->name('index');
+    });
+
+    Route::prefix('series')->name('series.')->group(function () {
+        Route::get('/', [SeriesController::class, 'index'])->name('index');
+        Route::get('/create', [SeriesController::class, 'create'])->name('create');
+        Route::post('/store', [SeriesController::class, 'store'])->name('store');
+        Route::get('/{series:slug}', [SeriesController::class, 'show'])->name('show');
+        Route::get('/edit/{series:slug}', [SeriesController::class, 'edit'])->name('edit');
+        Route::patch('/update/{series:slug}', [SeriesController::class, 'update'])->name('index');
+        Route::delete('/destroy/{series:slug}', [SeriesController::class, 'destroy'])->name('index');
+    });
+
+    Route::prefix('episode')->name('episode.')->group(function () {
+        Route::get('/', [EpisodeController::class, 'index'])->name('index');
+        Route::get('/create', [EpisodeController::class, 'create'])->name('create');
+        Route::post('/store', [EpisodeController::class, 'store'])->name('store');
+        Route::get('/{episode:slug}', [EpisodeController::class, 'show'])->name('show');
+        Route::get('/edit/{episode:slug}', [EpisodeController::class, 'edit'])->name('edit');
+        Route::patch('/update/{episode:slug}', [EpisodeController::class, 'update'])->name('index');
+        Route::delete('/destroy/{episode:slug}', [EpisodeController::class, 'destroy'])->name('index');
+    });
+
+    Route::prefix('exercise')->name('exercise.')->group(function () {
+        Route::get('/', [ExerciseController::class, 'index'])->name('index');
+        Route::get('/create', [ExerciseController::class, 'create'])->name('create');
+        Route::post('/store', [ExerciseController::class, 'store'])->name('store');
+        Route::get('/{exercise:slug}', [ExerciseController::class, 'show'])->name('show');
+        Route::get('/edit/{exercise:slug}', [ExerciseController::class, 'edit'])->name('edit');
+        Route::patch('/update/{exercise:slug}', [ExerciseController::class, 'update'])->name('index');
+        Route::delete('/destroy/{exercise:slug}', [ExerciseController::class, 'destroy'])->name('index');
+    });
+
+    Route::prefix('lesson')->name('lesson.')->group(function () {
+        Route::get('/', [LessonController::class, 'index'])->name('index');
+        Route::get('/create', [LessonController::class, 'create'])->name('create');
+        Route::post('/store', [LessonController::class, 'store'])->name('store');
+        Route::get('/{lesson:slug}', [LessonController::class, 'show'])->name('show');
+        Route::get('/edit/{lesson:slug}', [LessonController::class, 'edit'])->name('edit');
+        Route::patch('/update/{lesson:slug}', [LessonController::class, 'update'])->name('index');
+        Route::delete('/destroy/{lesson:slug}', [LessonController::class, 'destroy'])->name('index');
+    });
 });
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
