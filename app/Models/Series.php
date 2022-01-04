c92c12ceaed2701b1c78e27c8a3b5adab3bcab40
Anto Wiranto <antowrnto11@gmail.com>
2022-01-04 08:23:11 +0000

    install algolia laravel

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Series extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $guarded = [];
 
